f8cf9ca08a31f7a5e4afec539fbb0cddf0d9c4a3
benjamin333sz <b3szurek@gmail.com>
2026-02-12 09:57:16 +0100

    Partie_1_complete

--- Partie_1/chefbot.py
+++ Partie_1/chefbot.py
@@ -8,33 +8,41 @@ load_dotenv()
 groq_client = Groq()
 langfuse = get_client()
 
-@observe(name="Groupe_SZUREK_KUSNIEREK_GOSSELIN")
+@observe(name="Groupe_SZUREK_KUSNIEREK_GOSSELIN_Partie_1")
 def ask_chef(question:str,temperature:float)->str:
-    response = groq_client.chat.completions.create(
-        model="openai/gpt-oss-120b",
-        messages=[
-            {
-                "role": "system",
-                "content": 
-                """
-                Tu es un chef cuisinier français spécialisé en cuisine de saison
-                """
-            },
-            {"role": "user", "content": question}
-        ],
-        temperature=temperature
-        )
+    with propagate_attributes(tags=["Partie_1","Groupe_SZUREK_KUSNIEREK_GOSSELIN"]):
+        try:
+            response = groq_client.chat.completions.create(
+                model="openai/gpt-oss-120b",
+                messages=[
+                    {
+                        "role": "system",
+                        "content": 
+                        """
+                        Tu es un chef cuisinier français spécialisé en cuisine de saison
+                        """
+                    },
+                    {"role": "user", "content": question}
+                ],
+                temperature=temperature
+                )
 
 
-    get_client().update_current_span(
-        metadata={"type": "response",
-                  "season":"winter",
-                  "output":response.choices[0].message.content,
-                  "temperature":temperature,
-                  "partie":"1",
-                  }
-    )
-
+            get_client().update_current_span(
+                metadata={
+                        "type": "response",
+                        "season":"winter",
+                        "output":response.choices[0].message.content,
+                        "temperature":temperature,
+                        "partie":"1",
+                        "status": "success"
+                        }
+            )
+        except Exception as e:
+            get_client().update_current_span(
+                level="ERROR",
+                status_message=str(e)
+            )
     return response.choices[0].message.content
 
 prompt="""
@@ -46,6 +54,7 @@ for temperature in temperatures:
           "_"*50,
           ask_chef(question=prompt,temperature=temperature),
           end="\n\n")
-    
+    # Pour température de 0.1, on a peu de créativité, alors que pour des températures plus élevé, nous avons plus de créativité (globalement)
+
 
 langfuse.flush()
